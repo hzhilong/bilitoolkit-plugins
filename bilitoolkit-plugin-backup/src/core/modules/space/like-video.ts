1d496531af32aec8efa28f04610b970856b8c86b
hzhilong <hzhilong777@outlook.com>
2026-09-10 02:28:39 +0800

    [哔哩备份姬]：修复bug

--- bilitoolkit-plugin-backup/src/core/modules/space/like-video.ts
+++ bilitoolkit-plugin-backup/src/core/modules/space/like-video.ts
@@ -1,20 +1,21 @@
 import { type DataType, DataTypeMap } from '@/core/types/data-type'
 import type { ExecuteContext } from '@/core/types/execute'
 import { OnlyClearableModule } from '@/core/modules/only-clearable-module'
-import { sleepRandom, getErrorMessage } from '@ybgnb/utils'
+import { sleepRandom, getErrorMessage, createAbortError } from '@ybgnb/utils'
 
 export class LikeVideoModule extends OnlyClearableModule {
   dataType: DataType = 'like_video'
   dataTypeName: string = DataTypeMap[this.dataType].name
 
   async clearData(context: ExecuteContext): Promise<string | void> {
-    const { client, onProgress, user } = context
+    const { client, onProgress, user, signal } = context
 
     const getLikeVideos = async () => {
       return client.api.get('https://api.bilibili.com/x/space/like/video', {
         query: {
           vmid: user.mid,
         },
+        signal,
       })
     }
 
@@ -31,10 +32,13 @@ export class LikeVideoModule extends OnlyClearableModule {
       for (let i = 0; i < likeVideos.length; i++) {
         const video = likeVideos[i]
         try {
-          await client.videoAction.like({
-            aid: video.aid,
-            like: false,
-          })
+          await client.videoAction.like(
+            {
+              aid: video.aid,
+              like: false,
+            },
+            { signal },
+          )
           onProgress?.((i * 100) / likeVideos.length, `已取消点赞：${video.title}`)
         } catch (e) {
           const msg = getErrorMessage(e)
@@ -43,6 +47,7 @@ export class LikeVideoModule extends OnlyClearableModule {
           }
           onProgress?.((i * 100) / likeVideos.length, `${msg}`)
         }
+        if (signal?.aborted) throw createAbortError()
         await sleepRandom(1222, 2233)
       }
     }
